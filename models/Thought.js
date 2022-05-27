a96320ab78efa5d5d10a97a49825f8d7234522ed
Ted J <tedmjablonowski@gmail.com>
2022-05-27 06:10:33 -0400

    Models and Controllers

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,7 @@
 const { Schema, model, Types } = require("mongoose");
 const dateFormat = require("../utils/date-format");
 
+// Reaction Schema
 const reactionSchema = new Schema(
   {
     reactionId: {
